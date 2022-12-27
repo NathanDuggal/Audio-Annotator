1462cb59ec7f619684d0f1549d19f3f0a53a8b77
NathanDuggal <39681791+NathanDuggal@users.noreply.github.com>
2022-12-27 11:13:45 -0800

    Correctly points to callback after authorization

--- src/examples/authorization_example.js
+++ src/examples/authorization_example.js
@@ -19,7 +19,7 @@ const CLIENT_SECRET = process.env.CLIENT_SECRET || 'none';
 
 var client_id = CLIENT_ID; // Your client id
 var client_secret = CLIENT_SECRET; // Your secret
-var redirect_uri = 'https://open.spotify.com/'; // Your redirect uri
+var redirect_uri = 'http://localhost:8888/callback'; // Your redirect uri
 
 /**
  * Generates a random string containing numbers and letters
